bc795126f807d69cea027b6fd8552e33bbddd205
Artem Khachatryan <akhachatryan@aimtech.com>
2019-04-21 15:15:19 +0300

    add api to init

--- app/__init__.py
+++ app/__init__.py
@@ -6,3 +6,8 @@ from flask_sqlalchemy import SQLAlchemy
 app = Flask(__name__)
 app.config.from_object('config')
 db = SQLAlchemy(app)
+
+from app.api.locations import api_locations
+app.register_blueprint(api_locations, url_prefix='/api')
+
+from app import models
