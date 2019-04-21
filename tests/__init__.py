41dfb65ab92cd7487c61ff035085a4a45ffa7c36
snusmumrik <andarpolis@gmail.com>
2019-04-21 23:57:47 +0300

    switch to pytest

--- tests/__init__.py
+++ tests/__init__.py
@@ -1,10 +1,1 @@
 # -*- coding: utf-8 -*-
-from tests import config
-import unittest
-
-
-def run():
-    for test in config.tests:
-        print(test().get_desc())
-        suite = unittest.TestLoader().loadTestsFromTestCase(test)
-        unittest.TextTestRunner(verbosity=2).run(suite)
--- tests/config.py
+++ tests/config.py
@@ -2,8 +2,10 @@
 import tests.db.locations
 
 import tests.api.locations
+import tests.api.routers
 
 tests = [
     tests.db.locations.TestCase,
     tests.api.locations.TestCase,
+    tests.api.routers.TestCase,
 ]
