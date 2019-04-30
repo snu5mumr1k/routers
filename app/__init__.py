004c4eea7206662d8ccf0741b1f64b1e9aa10662
Artem Khachatryan <akhachatryan@aimtech.com>
2019-04-30 10:36:18 +0300

    flake8 fixes

--- app/__init__.py
+++ app/__init__.py
@@ -16,6 +16,7 @@ app.register_blueprint(api_locations, url_prefix='/api')
 from app.api.routers import api_routers  # noqa: E402
 app.register_blueprint(api_routers, url_prefix='/api')
 
+
 @babel.localeselector
 def get_locale():
     return app.config['LANGUAGE']
